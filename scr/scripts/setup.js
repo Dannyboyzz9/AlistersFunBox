78836a587ab2d945d8bec8e5a4cb699918c279da
Battleboyzz9 <44595864+Battleboyzz9@users.noreply.github.com>
2021-10-29 15:04:38 +1300

    Non Testing Code Done

--- scr/scripts/setup.js
+++ scr/scripts/setup.js
@@ -9,12 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById("save-button").addEventListener('click',function() {saveTrip();});
         document.getElementById("load-button").addEventListener('click',function() {loadTrip();});
         document.getElementById("exit").addEventListener('click',function() {exit();});
-        document.getElementById("add-button").addEventListener('click',function() {addDest(getElement("search").value);});
-        document.getElementById("add-button").addEventListener('click',function() {destDistCalc();});
+        document.getElementById("add-button").addEventListener('click',function() {addDest(getElement("search").value);destDistCalc();});
         document.getElementById("search").addEventListener('keyup',function() {searchTOWN(this);});
         document.getElementById("destination-list").addEventListener('click',function(event) {selectDest(event.target)});
         document.getElementById("remove-button").addEventListener('click',function() {removeDest(getElement("selectDest"));});
-        document.getElementById("remove-button").addEventListener('click',function() {finalDest();});
         document.getElementById("calc-button").addEventListener('click',function() {fuelCost_click();});
     }
 
